ba80d56ab852fbdfcdf81562a4780b71664ca6d2
paweln <pawelnowaczek88@gmail.com>
2022-02-19 12:49:47 +0100

    Add jasypt to encode database pass

--- src/main/java/pl/naapp/boxrank/BoxrankApplication.java
+++ src/main/java/pl/naapp/boxrank/BoxrankApplication.java
@@ -1,9 +1,11 @@
 package pl.naapp.boxrank;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableEncryptableProperties
 public class BoxrankApplication {
 
     public static void main(String[] args) {
